a226308e4e282143528781c71302997cf71aacf3
Yair Eshel Cahansky <guruyaya@gmail.com>
2021-01-17 19:10:40 +0200

    Small cleaups

--- ebay_scraper_new.py
+++ ebay_scraper_new.py
@@ -5,14 +5,16 @@ import argparse
 from urllib.parse import quote_plus
 import re
 
-cleanr      = re.compile('<.*?>')
-re_par      = re.compile('\(([^\)]*)\)')
-re_percent  = re.compile('([0-9.]*)%')
+# Widly used regexes compiled
+re_html_cleanr = re.compile(r'<.*?>')
+re_par         = re.compile(r'\(([^\)]*)\)')
+re_percent     = re.compile(r'([0-9.]*)%')
+
 # enter multiple phrases separated by '',
 phrases =['samsung a7']
 
-def cleanhtml(raw_html):    
-    cleantext = re.sub(cleanr, '', raw_html)
+def clean_html(raw_html):    
+    cleantext = re.sub(re_html_cleanr, '', raw_html)
     return cleantext
 
 def get_in_paranthasis(str_par):
@@ -22,11 +24,11 @@ def get_percent(str_par):
     try:
         return re_percent.search(str_par)[1]
     except:
-        print (str_par)
+        print ("DEBUG", str_par)
         raise
     
 def get_seller_info_from_span(seller_persona_span):
-    print ("Num remarks:", get_in_paranthasis(cleanhtml( str(seller_persona_span.contents[1]) )))
+    print ("Num remarks:", get_in_paranthasis(clean_html( str(seller_persona_span.contents[1]) )))
     print ("precent: ", get_percent(str( seller_persona_span.contents[2] )))
     return
 
@@ -35,10 +37,10 @@ def get_data_from_seller_page(seller_span):
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, "lxml")
     
-    overall_rating = int( cleanhtml( str(soup.find(class_='overall-rating-summary').
+    overall_rating = int( clean_html( str(soup.find(class_='overall-rating-summary').
         contents[1].contents[1].contents[0].contents[3] # table->tbody->line1->cell4
     ) ) )
-    overall_rating += int( cleanhtml( str(soup.find(class_='overall-rating-summary').
+    overall_rating += int( clean_html( str(soup.find(class_='overall-rating-summary').
         contents[1].contents[1].contents[2].contents[3] # table->tbody->line3->cell4
     ) ) )
     print ("overall_rating:", overall_rating)

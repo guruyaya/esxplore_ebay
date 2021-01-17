a931b0d0c1165a679dc29888c026b3fae3d82075
Yair Eshel Cahansky <guruyaya@gmail.com>
2021-01-17 20:22:39 +0200

    Code fixes

--- ebay_scraper_new.py
+++ ebay_scraper_new.py
@@ -20,12 +20,8 @@ def clean_html(raw_html):
 def get_in_paranthasis(str_par):
     return re_par.search(str_par)[1]
 
-def get_percent(str_par):
-    try:
-        return re_percent.search(str_par)[1]
-    except:
-        print ("DEBUG", str_par)
-        raise
+def get_percent(str_per):
+    return re_percent.search(str_per)[1]
     
 def get_seller_info_from_persona(seller_persona_span):
     print ("Num remarks:", get_in_paranthasis(clean_html( str(seller_persona_span.contents[1]) )))
@@ -33,21 +29,27 @@ def get_seller_info_from_persona(seller_persona_span):
     return
 
 def get_data_from_seller_page(seller_span):
-    res = requests.get(seller_span.contents[1]['href'])
+    seller_page_href = seller_span.contents[1]['href']
+    res = requests.get(seller_page_href)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, "lxml")
     
-    overall_rating = int( clean_html( str(soup.find(class_='overall-rating-summary').
-        contents[1].contents[1].contents[0].contents[3] # table->tbody->line1->cell4
-    ) ) )
-    overall_rating += int( clean_html( str(soup.find(class_='overall-rating-summary').
-        contents[1].contents[1].contents[2].contents[3] # table->tbody->line3->cell4
-    ) ) )
-    print ("overall_rating:", overall_rating)
+    rating = soup.find(**{'data-test-id':"user-score"}).contents[0]
+    print ("rating_score:", rating)
 
-    positive_feedback = get_percent( str(soup.find(class_='positiveFeedbackText')) )
-    print ("positive_feedback:", positive_feedback)
-    return
+    all_votes = 0
+
+    for i in range(3):
+        all_votes += int( clean_html( str(soup.find(class_='overall-rating-summary').
+            contents[1].contents[1].contents[i].contents[3] # table->tbody->line-i->cell-4 (count for 12 month)
+    ) ) )
+    print ("overall_votes:", all_votes)
+
+    positive_feedback = float(get_percent( str(soup.find(class_='positiveFeedbackText')) ))
+    print ("positive_feedback: {:.2f}".format(positive_feedback))
+
+
+    return (rating, all_votes, positive_feedback)
 
 def explore_product_page(href):
     res = requests.get(href)
@@ -61,10 +63,6 @@ def explore_product_page(href):
         res.raise_for_status()
         soup = bs4.BeautifulSoup(res.text, "lxml")
 
-    seller_persona_span = soup.find(class_='seller-persona')
-    if seller_persona_span:
-        return get_seller_info_from_persona(seller_persona_span)
-
     seller_span = soup.find(class_='mbg-l')
     if seller_span:
         return get_data_from_seller_page(seller_span)
@@ -73,14 +71,11 @@ def explore_product_page(href):
     if seller_span:
         return get_data_from_seller_page(seller_span)
         
-    
-    original_listing = soup.find_all('h1')
-    print (original_listing)
-    raise Exception("Stop!")
-
-
 for phrase in phrases:
-    site = 'https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_udlo=&_udhi=&LH_Auction=1&_samilow=&_samihi=&_sadis=15&_stpos=90278-4805&_fosrp=1&LH_Complete=1&_nkw={}&_pppn=r1&scp=ce0'.format(quote_plus(phrase))
+    site = ('https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_udlo=' +
+            '&_udhi=&LH_Auction=1&_samilow=&_samihi=&_sadis=15&_stpos=90278-4805'+
+            '&_fosrp=1&LH_Complete=1&_nkw={}'+
+            '&_pppn=r1&scp=ce0').format(quote_plus(phrase))
 
     res = requests.get(site)
     res.raise_for_status()
@@ -99,6 +94,7 @@ for phrase in phrases:
         except:
             print (href)
             raise
+
     if i > 10:
         break
 

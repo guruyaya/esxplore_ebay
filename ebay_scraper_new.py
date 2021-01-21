2a65849347450c1b2cc5f002886617b47c7bfbc6
Yair Eshel Cahansky <guruyaya@gmail.com>
2021-01-21 16:01:40 +0200

    Fixed f1 dates to handle members from the 90s

--- ebay_scraper_new.py
+++ ebay_scraper_new.py
@@ -12,7 +12,7 @@ import traceback
 # Widly used regexes compiled
 re_par         = re.compile(r'\(([^\)]*)\)')
 re_percent     = re.compile(r'([0-9.]*)%')
-re_date_f1     = re.compile(r'[A-z][a-z][a-z]-[0-3][0-9]-[0-2][0-9]')
+re_date_f1     = re.compile(r'[A-z][a-z][a-z]-[0-3][0-9]-[0-9][0-9]')
 re_date_f2     = re.compile(r'[0-3]?[0-9] [A-z][a-z][a-z] [1-2][09][0-9][0-9] at [1-2]?[0-9]')
 
 # enter multiple phrases separated by '',

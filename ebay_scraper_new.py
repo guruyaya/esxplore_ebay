5173df12b21846956bc7aa60acd9292937d6e2e7
Yair Eshel Cahansky <guruyaya@gmail.com>
2021-01-21 23:53:04 +0200

    added phrases to all phones

--- ebay_scraper_new.py
+++ ebay_scraper_new.py
@@ -16,7 +16,12 @@ re_date_f1     = re.compile(r'[A-z][a-z][a-z]-[0-3][0-9]-[0-9][0-9]')
 re_date_f2     = re.compile(r'[0-3]?[0-9] [A-z][a-z][a-z] [1-2][09][0-9][0-9] at [1-2]?[0-9]')
 
 # enter multiple phrases separated by '',
-phrases = ['iphone 6s', 'iphone 6 -6s']
+phrases = ('Iphone 1','iphone 3G','iphone 3G -3Gs','Iphone 4 -4s',
+        'Iphone 4s','Iphone 5 -5c -5s','iphone 5c','Iphone 5s','Iphone 6 -plus -6s',
+        'Iphone 6 plus -6s','iphone 6s -plus','iphone 6s plus','Iphone se','Iphone 7 -plus',
+        'Iphone 7 plus','Iphone 8 -plus','Iphone 8 plus','iphone x -xs -max -xr',
+        'Iphone xs -max','Iphone xs max','Iphone xr','Iphone 11 –pro -max',
+        'Iphone 11 pro -max','Iphone 11 pro max',)
 
 
 def get_total_pages(given_url):
